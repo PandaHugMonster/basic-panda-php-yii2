5d576dc37bed087c84bc7f333734cd551cdbe733
Ivan Ponomarev <i.ponomarev@pandev.tech>
2024-07-06 10:31:50 +0200

    Small fixes for 2.0.1

--- app/bin/in-container-entrypoint.sh
+++ app/bin/in-container-entrypoint.sh
@@ -1,25 +1,14 @@
 #!/bin/sh
 
-_STAGE="${1}";
-
 cd /app || (printf "?? App folder is missing. Exiting.\n" && exit 1);
 
-printf "## Updating Composer (%s)\n" "${_STAGE}";
+printf "## Updating Composer (%s)\n" "${APP_STAGE}";
 
-if [ "${_STAGE}" = "dev" ]
+if [ "${APP_STAGE}" = "dev" ]
 then
 	composer install;
-else
-
-	php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');";
-    php -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;";
-    php composer-setup.php;
-    php -r "unlink('composer-setup.php');";
-
-    mv composer.phar /bin/composer;
-
-	composer install;
 fi;
+
 
 printf "## Start App Container\n";
 
